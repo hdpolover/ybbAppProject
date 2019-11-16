72e7ff5a5634e5e8ffb53fced0fceb91f00b38d9
Ronald Arrival Fajar <ronaldarrivalf@gmail.com>
2019-11-16 19:38:18 +0700

    change chatlist

--- app/src/main/java/com/hdpolover/ybbproject/MessagesFragment.java
+++ app/src/main/java/com/hdpolover/ybbproject/MessagesFragment.java
@@ -38,7 +38,7 @@ import java.util.List;
 public class MessagesFragment extends Fragment {
 
     RecyclerView recyclerView;
-    AdapterUsers adapterUsers;
+    //AdapterUsers adapterUsers;
     List<ModelChatlist> chatList;
     List<ModelUser> userList;
 
@@ -151,81 +151,81 @@ public class MessagesFragment extends Fragment {
             }
         });
     }
-
-    private void getAllUsers() {
-        //get current user
-        final FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
-        //get path of database name "Users" containing users info
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
-        //get all data from path
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                userList.clear();
-                for(DataSnapshot ds: dataSnapshot.getChildren()){
-                    ModelUser modelUser = ds.getValue(ModelUser.class);
-
-                    //get all users except currently signed in user
-                    if(!modelUser.getUid().equals(fUser.getUid())){
-                        userList.add(modelUser);
-                    }
-
-                    //adapter
-                    adapterUsers = new AdapterUsers(getActivity(), userList);
-                    //set adapter to recycler view
-                    recyclerView.setAdapter(adapterUsers);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-    }
-
-    private void searchUsers(final String query) {
-        //get current user
-        final FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
-        //get path of database name "Users" containing users info
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
-        //get all data from path
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                userList.clear();
-                for(DataSnapshot ds: dataSnapshot.getChildren()){
-                    ModelUser modelUser = ds.getValue(ModelUser.class);
-
-                    //conditions to fulfil search
-                    // 1. User not current user
-                    // 2. The user name of email contains text entered in Searchview (case insersitive)
-
-                    //get all searched users except currently signed in user
-
-                    if(!modelUser.getUid().equals(fUser.getUid())){
-                        if(modelUser.getName().toLowerCase().contains(query.toLowerCase()) ||
-                                modelUser.getEmail().toLowerCase().contains(query.toLowerCase())){
-                            userList.add(modelUser);
-                        }
-                    }
-
-                    //adapter
-                    adapterUsers = new AdapterUsers(getActivity(), userList);
-                    //refresh adapter
-                    adapterUsers.notifyDataSetChanged();
-                    //set adapter to recycler view
-                    recyclerView.setAdapter(adapterUsers);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
+//
+//    private void getAllUsers() {
+//        //get current user
+//        final FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+//        //get path of database name "Users" containing users info
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+//        //get all data from path
+//        ref.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                userList.clear();
+//                for(DataSnapshot ds: dataSnapshot.getChildren()){
+//                    ModelUser modelUser = ds.getValue(ModelUser.class);
+//
+//                    //get all users except currently signed in user
+//                    if(!modelUser.getUid().equals(fUser.getUid())){
+//                        userList.add(modelUser);
+//                    }
+//
+//                    //adapter
+//                    adapterUsers = new AdapterUsers(getActivity(), userList);
+//                    //set adapter to recycler view
+//                    recyclerView.setAdapter(adapterUsers);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
+//    }
+//
+//    private void searchUsers(final String query) {
+//        //get current user
+//        final FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+//        //get path of database name "Users" containing users info
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+//        //get all data from path
+//        ref.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                userList.clear();
+//                for(DataSnapshot ds: dataSnapshot.getChildren()){
+//                    ModelUser modelUser = ds.getValue(ModelUser.class);
+//
+//                    //conditions to fulfil search
+//                    // 1. User not current user
+//                    // 2. The user name of email contains text entered in Searchview (case insersitive)
+//
+//                    //get all searched users except currently signed in user
+//
+//                    if(!modelUser.getUid().equals(fUser.getUid())){
+//                        if(modelUser.getName().toLowerCase().contains(query.toLowerCase()) ||
+//                                modelUser.getEmail().toLowerCase().contains(query.toLowerCase())){
+//                            userList.add(modelUser);
+//                        }
+//                    }
+//
+//                    //adapter
+//                    adapterUsers = new AdapterUsers(getActivity(), userList);
+//                    //refresh adapter
+//                    adapterUsers.notifyDataSetChanged();
+//                    //set adapter to recycler view
+//                    recyclerView.setAdapter(adapterUsers);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
     private void checkUserStatus() {
         //get current user
@@ -250,54 +250,54 @@ public class MessagesFragment extends Fragment {
 
     //Inflate options menu
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        //inflating menu
-        inflater.inflate(R.menu.menu_main, menu);
-
-        //hide searchview, as we dont need it here
-       menu.findItem(R.id.action_logout).setVisible(false);
-        menu.findItem(R.id.action_add_post).setVisible(false);
-
-        //searchView
-        MenuItem item = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
-
-        //search listener
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                //called when user press search button from keyboard
-                //if search query is not empty the search
-                if(!TextUtils.isEmpty(s.trim())){
-                    //search text contains text, search it
-                    searchUsers(s);
-                }
-                else{
-                    //search text empty, get all users
-                    getAllUsers();
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                //called whenever user press any single letter
-                //if search query is not empty the search
-                if(!TextUtils.isEmpty(s.trim())){
-                    //search text contains text, search it
-                    searchUsers(s);
-                }
-                else{
-                    //search text empty, get all users
-                    getAllUsers();
-                }
-                return false;
-            }
-        });
-
-        super.onCreateOptionsMenu(menu, inflater);
-    }
+//    @Override
+//    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+//        //inflating menu
+//        inflater.inflate(R.menu.menu_main, menu);
+//
+//        //hide searchview, as we dont need it here
+//       menu.findItem(R.id.action_logout).setVisible(false);
+//        menu.findItem(R.id.action_add_post).setVisible(false);
+//
+//        //searchView
+//        MenuItem item = menu.findItem(R.id.action_search);
+//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+//
+//        //search listener
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                //called when user press search button from keyboard
+//                //if search query is not empty the search
+//                if(!TextUtils.isEmpty(s.trim())){
+//                    //search text contains text, search it
+//                    searchUsers(s);
+//                }
+//                else{
+//                    //search text empty, get all users
+//                    getAllUsers();
+//                }
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String s) {
+//                //called whenever user press any single letter
+//                //if search query is not empty the search
+//                if(!TextUtils.isEmpty(s.trim())){
+//                    //search text contains text, search it
+//                    searchUsers(s);
+//                }
+//                else{
+//                    //search text empty, get all users
+//                    getAllUsers();
+//                }
+//                return false;
+//            }
+//        });
+//
+//        super.onCreateOptionsMenu(menu, inflater);
+//    }
 
     //handle menu item click
 
