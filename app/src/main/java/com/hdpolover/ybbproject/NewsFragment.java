5697e88715baad916ae21c715f938bf3d77d3fb2
hdpolover <hendrapolover@gmail.com>
2019-11-27 00:03:25 +0700

    updated schedules a bit

--- app/src/main/java/com/hdpolover/ybbproject/NewsFragment.java
+++ app/src/main/java/com/hdpolover/ybbproject/NewsFragment.java
@@ -9,12 +9,34 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.hdpolover.ybbproject.adapters.AdapterProfile;
 
 public class NewsFragment extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    AdapterProfile viewAdapterProfile;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+        /////*     initialize view   */////
+        viewPager = view.findViewById(R.id.viewPager);
+
+        /////*     initialize ViewPager   */////
+        viewAdapterProfile = new AdapterProfile(getFragmentManager());
+
+        /////*     add adapter to ViewPager  */////
+        viewPager.setAdapter(viewAdapterProfile);
+        tabLayout = view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabRippleColor(null);
+
+        return view;
     }
 
 }
