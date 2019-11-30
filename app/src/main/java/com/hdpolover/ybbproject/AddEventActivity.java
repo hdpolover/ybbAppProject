f7dcf1e1ea84ddef80e090443a626a9debec269e
hdpolover <hendrapolover@gmail.com>
2019-11-30 23:30:35 +0700

    updated addeventactivity

--- app/src/main/java/com/hdpolover/ybbproject/AddEventActivity.java
+++ app/src/main/java/com/hdpolover/ybbproject/AddEventActivity.java
@@ -3,17 +3,27 @@ package com.hdpolover.ybbproject;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity {
 
@@ -21,6 +31,9 @@ public class AddEventActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
 
     String uid;
+
+    EditText titleEt, descEt, dateEt;
+    TextInputLayout eventDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +47,18 @@ public class AddEventActivity extends AppCompatActivity {
         actionBar.setTitle("Create New Event");
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        titleEt = findViewById(R.id.titleEt);
+        descEt = findViewById(R.id.descEt);
+        dateEt = findViewById(R.id.dateEt);
+        eventDate = findViewById(R.id.eventDate);
+
+        eventDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleDateButton();
+            }
+        });
     }
 
     private void checkUserStatus() {
@@ -42,6 +67,55 @@ public class AddEventActivity extends AppCompatActivity {
         } else {
             //startActivity(new Intent(SettingsActivity.this, MainActivity.class));
         }
+    }
+
+    private void handleDateButton() {
+        Calendar calendar = Calendar.getInstance();
+        int YEAR = calendar.get(Calendar.YEAR);
+        int MONTH = calendar.get(Calendar.MONTH);
+        int DATE = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.YEAR, year);
+                calendar1.set(Calendar.MONTH, month);
+                calendar1.set(Calendar.DATE, date);
+                String dateText = DateFormat.format("EEEE, MMM d, yyyy", calendar1).toString();
+
+                dateEt.setText(dateText);
+            }
+        }, YEAR, MONTH, DATE);
+
+        datePickerDialog.show();
+
+
+
+
+    }
+
+    private void handleTimeButton() {
+        Calendar calendar = Calendar.getInstance();
+        int HOUR = calendar.get(Calendar.HOUR);
+        int MINUTE = calendar.get(Calendar.MINUTE);
+        boolean is24HourFormat = DateFormat.is24HourFormat(this);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                //Log.i(TAG, "onTimeSet: " + hour + minute);
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.HOUR, hour);
+                calendar1.set(Calendar.MINUTE, minute);
+                String dateText = DateFormat.format("h:mm a", calendar1).toString();
+                //timeTextView.setText(dateText);
+            }
+        }, HOUR, MINUTE, is24HourFormat);
+
+        timePickerDialog.show();
+
     }
 
     @Override
