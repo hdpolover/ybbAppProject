4e13e50fd9b5fd4161afb357868cdddd4d49490e
hdpolover <hendrapolover@gmail.com>
2019-12-17 08:37:33 +0700

    updated app version

--- app/src/main/java/com/hdpolover/ybbproject/SettingsActivity.java
+++ app/src/main/java/com/hdpolover/ybbproject/SettingsActivity.java
@@ -6,25 +6,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.hdpolover.ybbproject.helpers.AppVersionCheck;
 
 public class SettingsActivity extends AppCompatActivity {
 
     FirebaseUser firebaseUser;
     FirebaseAuth firebaseAuth;
 
+    AppVersionCheck appVersionCheck;
+
     String uid;
 
     MaterialCardView logoutBtn, aboutBtn, feedbackBtn;
+    TextView ybbVersionTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        appVersionCheck = new AppVersionCheck();
 
         firebaseAuth = firebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
@@ -40,6 +47,9 @@ public class SettingsActivity extends AppCompatActivity {
         logoutBtn = findViewById(R.id.logoutBtn);
         feedbackBtn = findViewById(R.id.feedbackBtn);
         aboutBtn = findViewById(R.id.aboutBtn);
+        ybbVersionTv = findViewById(R.id.ybbVersionTv);
+
+        ybbVersionTv.setText(getResources().getString(R.string.app_name) + " " + appVersionCheck.getAppVersion());
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/hdpolover/ybbproject/helpers/AppVersionCheck.java
@@ -0,0 +1,10 @@
+package com.hdpolover.ybbproject.helpers;
+
+import com.hdpolover.ybbproject.BuildConfig;
+
+public class AppVersionCheck {
+
+    public String getAppVersion() {
+        return BuildConfig.VERSION_NAME;
+    }
+}
