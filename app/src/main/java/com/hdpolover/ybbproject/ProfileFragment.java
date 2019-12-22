64aa6eae1cf28788cba47671511c1fa151284256
Ronald Arrival Fajar <ronaldarrivalf@gmail.com>
2019-12-22 21:28:52 +0700

    disable toast in profile

--- app/src/main/java/com/hdpolover/ybbproject/ProfileFragment.java
+++ app/src/main/java/com/hdpolover/ybbproject/ProfileFragment.java
@@ -525,7 +525,7 @@ public class ProfileFragment extends Fragment {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(getActivity(), "" + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(), "" + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -564,7 +564,7 @@ public class ProfileFragment extends Fragment {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(getActivity(), "" + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(), "" + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
